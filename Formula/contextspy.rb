6eaf1a53ba88d17e80efaf7b8b54efbc00003019
Rimantas Z <rimasz@gmail.com>
2026-05-20 01:56:10 +0300

    fixed typo in repo name

--- Formula/contextspy.rb
+++ Formula/contextspy.rb
@@ -1,6 +1,6 @@
 class Contextspy < Formula
   desc "LLM proxy that analyses token usage in context windows"
-  homepage "https://github.com/rimantas/contextspy"
+  homepage "https://github.com/RimantasZ/contextspy"
   version "0.1.0"
   license "Apache-2.0"
 
@@ -9,18 +9,18 @@ class Contextspy < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/rimantas/contextspy/releases/download/v#{version}/contextspy-macos-arm64.tar.gz"
+      url "https://github.com/RimantasZ/contextspy/releases/download/v#{version}/contextspy-macos-arm64.tar.gz"
       sha256 "REPLACE_ARM64_SHA256"
     end
     on_intel do
-      url "https://github.com/rimantas/contextspy/releases/download/v#{version}/contextspy-macos-x86_64.tar.gz"
+      url "https://github.com/RimantasZ/contextspy/releases/download/v#{version}/contextspy-macos-x86_64.tar.gz"
       sha256 "REPLACE_X86_64_SHA256"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/rimantas/contextspy/releases/download/v#{version}/contextspy-linux-x86_64.tar.gz"
+      url "https://github.com/RimantasZ/contextspy/releases/download/v#{version}/contextspy-linux-x86_64.tar.gz"
       sha256 "REPLACE_LINUX_SHA256"
     end
   end
